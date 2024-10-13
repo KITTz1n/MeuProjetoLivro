75dc5d13b325e2929cb3df9b84cb2f26eb801193
Wellington <wellcomepudim@gmail.com>
2024-10-13 17:18:29 -0300

    muita att

---
+++ jsf/funcoes.js
@@ -0,0 +1,10 @@
+function net()
+{
+    window.location.href = "historias/índice.html"; //Redireciona a pessoa para outra aba
+}
+function pdf()
+{
+    alert('Ainda sendo desenvolvido')
+    console.log('PDF CLICADO')
+    // FAZER INSTALAR O PDF DO LIVRO
+}

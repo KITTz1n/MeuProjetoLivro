1eafa5ce78f6c26f5d4ea8c8629a64cc84a62557
Wellington <wellcomepudim@gmail.com>
2024-10-13 21:16:50 -0300

    mais att

--- jsf/funcoes.js
+++ jsf/funcoes.js
@@ -8,3 +8,14 @@ function pdf()
     console.log('PDF CLICADO')
     // FAZER INSTALAR O PDF DO LIVRO
 }
+function Desenvolvendo()
+{
+    alert('Ainda sendo desenvolvido')
+    console.log('Tentou algo em desenvolvimento ainda')
+    //Essa funcao serve para tudo aquilo que estiver em desenvolvimento ser chamada ela
+}
+function retro(local)
+{
+    var vamo = '../' + String(local) + '.html';
+    window.location.href = vamo
+}

7d4fe9f57d49efefe6bdac376218540249b8adab
Wellington <wellcomepudim@gmail.com>
2024-10-15 14:42:45 -0300

    att

--- jsf/funcoes.js
+++ jsf/funcoes.js
@@ -1,21 +1,17 @@
-function net()
-{
-    window.location.href = "historias/índice.html"; //Redireciona a pessoa para outra aba
-}
-function pdf()
-{
-    alert('Ainda sendo desenvolvido')
-    console.log('PDF CLICADO')
-    // FAZER INSTALAR O PDF DO LIVRO
-}
 function Desenvolvendo()
 {
     alert('Ainda sendo desenvolvido')
     console.log('Tentou algo em desenvolvimento ainda')
     //Essa funcao serve para tudo aquilo que estiver em desenvolvimento ser chamada ela
 }
-function retro(local)
+function retro_e_avan(local)
 {
-    var vamo = '../' + String(local) + '.html';
-    window.location.href = vamo
+    if (local == '')
+    {
+        alert('Ainda em desenvolvimento ou ocorreu algum erro!')
+    }
+    else
+    {
+        window.location.href = local
+    }
 }
